ccd7ef6e7c9abcad0ac1336b63567cfb2c491b3e
Rajkaran <rajkaranvir456@gmail.com>
2022-10-05 23:57:02 +0530

    Added CategorySection and offerSection to homepage

--- frontend/src/Components/Products.js
+++ frontend/src/Components/Products.js
@@ -24,7 +24,7 @@ export default function Products() {
             {/* <!-- Products Start --> */}
             <div class="container-fluid pt-5">
                 <div class="text-center mb-4">
-                    <h2 class="section-title px-5"><span class="px-2">Products</span></h2>
+                    <h2 class="section-title px-5"><span class="px-2">Our Top Products</span></h2>
                 </div>
                 <div class="row px-xl-5 pb-3">
                     {Object.keys(data).length > 0 ?
--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import CategoryProduct from '../Components/CategoryProduct'
 import FeaturesSection from '../Components/FeaturesSection'
 import Footer from '../Components/Footer'
 import Navbar from '../Components/Navbar'
+import OfferProduct from '../Components/OfferProduct'
 import Products from '../Components/Products'
 import Topbar from '../Components/Topbar'
 
@@ -11,6 +13,8 @@ export default function HomePage() {
         <Topbar />
         <Navbar />
         <FeaturesSection />
+        <CategoryProduct />
+        <OfferProduct />
         <Products />
         <Footer />
     </div>
