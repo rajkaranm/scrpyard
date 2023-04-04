575a71ca065a689808fbab515d9fc6e662b59aee
Rajkaran <rajkaranvir456@gmail.com>
2023-04-04 09:39:05 +0530

    solved product no found error

--- frontend/src/Pages/Search.js
+++ frontend/src/Pages/Search.js
@@ -21,42 +21,15 @@ export default function Search() {
   return (
     <div>
       <Topbar />
-      {/* {searchData.map((product) => (
-        <div className="col-lg-3 col-md-6 col-sm-12 pb-1">
-          <div className="card product-item border-0 mb-4">
-            <div className="card-header product-img position-relative overflow-hidden bg-transparent border p-0">
-              <img className="img-fluid w-100" src={product.imgUrl} alt="" />
-            </div>
-            <div className="card-body border-left border-right text-center p-0 pt-4 pb-3">
-              <h6 className="text-truncate mb-3">{product.productName}</h6>
-              <div className="d-flex justify-content-center">
-                <h6>&#8377; {product.price}</h6>
-                <h6 className="text-muted ml-2">
-                  <del>&#8377;{product.price + product.price * (10 / 100)}</del>
-                </h6>
-              </div>
-            </div>
-            <div className="card-footer d-flex justify-content-between bg-light border">
-              <a
-                href={`product/id/${product._id}`}
-                className="btn btn-sm text-dark p-0"
-              >
-                <i className="fas fa-eye text-primary mr-1"></i>View Detail
-              </a>
-              <a href="" className="btn btn-sm text-dark p-0">
-                <i className="fas fa-shopping-cart text-primary mr-1"></i>Add To
-                Cart
-              </a>
-            </div>
-          </div>
-        </div>
-      ))} */}
       <h3 style={{ textAlign: "center" }}>Results...</h3>
       <div style={{ margin: "1rem 10rem" }}>
-        {searchData &&
+        {searchData?.flag == 0 ? (
+          <div>No Results</div>
+        ) : (
+          searchData &&
           searchData.map((product) => {
             return (
-              <div key={product._id} >
+              <div key={product._id}>
                 <div className="col-lg-3 col-md-6 col-sm-12 pb-1">
                   <div className="card product-item border-0 mb-4">
                     <div className="card-header product-img position-relative overflow-hidden bg-transparent border p-0">
@@ -96,7 +69,8 @@ export default function Search() {
                 </div>
               </div>
             );
-          })}
+          })
+        )}
       </div>
       <Footer />
     </div>
